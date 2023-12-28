5b515dd9f503626a7d011cb0f57164a5a500971e
Eleazar Mercader <elmercaderdesundberg@gmail.com>
2023-12-28 15:32:57 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,17 +1,68 @@
 // create web server
-// 1. 导入 express
-const express = require('express')
-// 2. 创建 web 服务器
-const app = express()
-// 3. 注册一个路由
-app.get('/index', (req, res) => {
-  // res.send('欢迎来到首页')
-  // res.end('欢迎来到首页')
-  res.send({
-    name: 'zs',
-    age: 20
-  })
-})
-// 4. 启动服务器
-app.listen(3000, () => {
-  console.log('server running at http://
+// 1. load modules
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const qs = require('querystring');
+
+// 2. create server
+const server = http.createServer(function(request, response){
+  const parsedUrl = url.parse(request.url);
+  const resource = parsedUrl.pathname;
+  const query = parsedUrl.query;
+  const method = request.method;
+  console.log("resource=", resource);
+  console.log("method=", method);
+  console.log("query=", query);
+
+  if (method === 'GET'){
+    if (resource === '/'){
+      fs.readFile('./index.html', 'utf-8', function(error, data){
+        if (error){
+          response.writeHead(500, {'Content-Type':'text/html'});
+          response.end('500 Internal Server Error : ' + error);
+        } else {
+          response.writeHead(200, {'Content-Type':'text/html'});
+          response.end(data);
+        }
+      });
+    } else if (resource === '/comments'){
+      fs.readFile('./comments.json', 'utf-8', function(error, data){
+        if (error){
+          response.writeHead(500, {'Content-Type':'text/html'});
+          response.end('500 Internal Server Error : ' + error);
+        } else {
+          response.writeHead(200, {'Content-Type':'application/json'});
+          response.end(data);
+        }
+      });
+    } else {
+      response.writeHead(404, {'Content-Type':'text/html'});
+      response.end('404 Page Not Found');
+    }
+  } else if (method === 'POST'){
+    if (resource === '/comments'){
+      let body = "";
+      request.on('data', function(data){
+        body += data;
+      });
+      request.on('end', function(){
+        let comment = qs.parse(body);
+        console.log("comment=", comment);
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end(JSON.stringify(comment));
+      });
+    } else {
+      response.writeHead(404, {'Content-Type':'text/html'});
+      response.end('404 Page Not Found');
+    }
+  } else {
+    response.writeHead(404, {'Content-Type':'text/html'});
+    response.end('404 Page Not Found');
+  }
+});
+
+// 3. execute server
+server.listen(8080, function(){
+  console.log('Server is running...');
+});
